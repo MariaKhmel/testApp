4507944290c0042f9a900dc3e19ffb715dfd89ad
Maria Khmel <mariakhmel8@gmail.com>
2025-01-13 04:15:53 +0200

    Your commit message

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./App";
 
+const arr = [3, 4, 5, 6, 4, 3, 4, 5, 3, 4];
+const sortedArr = (array) => {
+  const copiedArr = [...array];
+  for (let i = 0; i < array.length; i += 1) {
+
+  }
+}
+
+console.log(sortedArr(arr));
+
 ReactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
